c27c7887344b06dcb4295dac4f98ef0491ee7887
Iheanyi Oziegbe <neoconstel@gmail.com>
2022-12-17 10:48:55 +0100

    remove default styles and set bootstrap 5.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is the Homepage</h1>
   </main>
 </template>
